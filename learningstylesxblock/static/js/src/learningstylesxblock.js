f2bac26a9c980bce7476aba48bd25f1b8f555386
danielpuliche <danielpuliche@unicauca.edu.co>
2023-01-31 20:04:45 -0500

    Update con DB

--- learningstylesxblock/static/js/src/learningstylesxblock.js
+++ learningstylesxblock/static/js/src/learningstylesxblock.js
@@ -7,20 +7,35 @@ function LearningStylesXBlock(runtime, element) {
 
     var handlerUrl = runtime.handlerUrl(element, 'get_formdata');
 
-    $('#Send', element).click(function(eventObject) {
-        var answers = [];
-        for (let i = 1; i <= 16; i++) {
-            answers.push($(`input[name="pregunta-${i}"]:checked`).val())   
-        }
+    $('#send', element).click(function(eventObject) {
         eventObject.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({
-                "answers": answers
-            }),
-            success: showResults
-        });
+        let answers = [];
+        let requiredFlag;
+        for (let i = 1; i <= 16; i++) {
+            if(!$(`input[name="pregunta-${i}"]:checked`).prop('checked')){
+                requiredFlag = true;
+                break;
+            }
+        }
+        if(requiredFlag){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Marca al menos una respuesta en todas las preguntas!',
+              });
+        }else{
+            for (let i = 1; i <= 16; i++) {
+                answers.push($(`input[name="pregunta-${i}"]:checked`).val())   
+            }
+            $.ajax({
+                type: "POST",
+                url: handlerUrl,
+                data: JSON.stringify({
+                    "answers": answers
+                }),
+                success: showResults
+            });
+        }
     });
 
     $(function ($) {
@@ -28,8 +43,13 @@ function LearningStylesXBlock(runtime, element) {
             let results = $('.results').map(function() {
                 return this.innerHTML;
             }).get();
-            graficarResultados(results);   
-            graficaD3(results);     
+            graficarResultados(results);                 
+        }
+        if($("#graficaB").length){
+            let results = $('.results').map(function() {
+                return this.innerHTML;
+            }).get();
+            graficarResultadosBarra(results);                 
         }
     });
 
@@ -37,7 +57,7 @@ function LearningStylesXBlock(runtime, element) {
         // Obtener una referencia al elemento canvas del DOM
         const $grafica = document.querySelector("#grafica");
         // Las etiquetas son las porciones de la gráfica
-        const etiquetas = ["Visual", "Audio", "Reading", "Kinestesico"]
+        const etiquetas = ["Auditive", "Kinesthetic", "Reading", "Visual" ];
         // Podemos tener varios conjuntos de datos. Comencemos con uno
         const datosIngresos = {
             data: results, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
@@ -57,48 +77,57 @@ function LearningStylesXBlock(runtime, element) {
             borderWidth: 1,// Ancho del borde
         };
         new Chart($grafica, {
-            type: 'pie',// Tipo de gráfica. Puede ser dougnhut o pie
+            type: 'polarArea',// Tipo de gráfica. Puede ser dougnhut o pie
             data: {
                 labels: etiquetas,
                 datasets: [
                     datosIngresos,
-                    // Aquí más datos...
+                ]
+            }            
+        });        
+    }
+    
+    function graficarResultadosBarra(results){
+        // Obtener una referencia al elemento canvas del DOM
+        const $graficaB = document.querySelector("#graficaB");
+        // Las etiquetas son las porciones de la gráfica
+        const etiquetasB = ["Auditive", "Kinesthetic", "Reading", "Visual" ];
+        // Podemos tener varios conjuntos de datos. Comencemos con uno
+        const datosIngresos = {
+            data: results, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+            // Ahora debería haber tantos background colors como datos, es decir, para este ejemplo, 4
+            backgroundColor: [
+                'rgb(240, 108, 100)',
+                'rgb(255, 215,0)',
+                'rgb(136, 206, 210)',
+                'rgb(32, 99, 155)',               
+            ],// Color de fondo
+            borderColor: [
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(0,0,0,1)',                
+            ],// Color del borde
+            borderWidth: 1,// Ancho del borde
+        };
+        new Chart($graficaB, {
+            type: 'bar',// Tipo de gráfica
+            data: {
+                labels: etiquetasB,
+                datasets: [
+                    datosIngresos,
                 ]
             },
-            animation: {
-                animateRotate: true
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                },
             }
         });        
     }
-
-    function graficaD3(results){
-        var data = results;
-        var width = 300;
-        var height = 300;
-        var radius = Math.min(width, height) / 2;
-
-        var color = d3.scaleOrdinal()
-            .range(["#ff0000", "#0000ff", "#00ff00", "#00ff00"]);
-
-        var arc = d3.arc()
-            .outerRadius(radius - 10)
-            .innerRadius(0);
-
-        var labelArc = d3.arc()
-            .outerRadius(radius - 40)
-            .innerRadius(radius - 40);
-
-        var pie = d3.pie()
-            .sort(null)
-            .value(function(d) { return d; });
-
-        var svg = d3.select("#grafica").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        var g = svg.selectAll(".arc")
-            .data
-    }
+    
 }
